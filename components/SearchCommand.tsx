c1b45d5d0eae97ce78dd6709b8264d0dab963a0e
aridepai17 <advaithdepai26@gmail.com>
2026-01-28 12:17:46 +0530

    fix(search): Use unique composite key for stock list items
    - Replace stock.symbol with composite key (symbol-exchange-index)
    
    - Prevents duplicate React key warnings for stocks on multiple exchanges

--- components/SearchCommand.tsx
+++ components/SearchCommand.tsx
@@ -109,7 +109,7 @@ export default function SearchCommand({
 								{` `}({displayStocks?.length || 0})
 							</div>
 							{displayStocks?.map((stock, i) => (
-								<li key={stock.symbol} className="search-item">
+								<li key={`${stock.symbol}-${stock.exchange}-${i}`} className="search-item">
 									<Link
 										href={`/stocks/${stock.symbol}`}
 										onClick={handleSelectStock}
